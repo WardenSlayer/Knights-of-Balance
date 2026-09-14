098062864576f28329ff852ae8971c8e42ed0fff
AzgalorHR <stephen.manion360@gmail.com>
2026-09-14 12:11:48 -0600

    Monk Cobra Fang
    Implemented original code to avoid complications

--- Knights  of Balance/knights_of_balance_beta.lua
+++ Knights  of Balance/knights_of_balance_beta.lua
@@ -1217,70 +1217,64 @@ function monk_wraps_of_strength_carddef()
     })
 end
 
+--=======================================
 function monk_cobra_fang_carddef()
-    -- This card draws 1 and grants 2 gold after your next acquisition.
+    local card_name = "monk_cobra_fang"
     local cardLayout = createLayout({
         name = "Cobra Fang",
         art = "art/classes/monk/monk_cobra_fang",
         frame = "frames/monk_frames/monk_action_cardframe",
-        cardTypeLabel = "Action",
-        xmlText =[[<vlayout spacing="20"> 
-                <text text="Gain 1 {combat} for each Tao Lu action in play." fontsize="26"/>
-                <text text="You count as having an extra Tao Lu action this turn." fontsize="26"/>
-                </vlayout>]]
+        xmlText = [[
+            <vlayout>
+<tmpro text="This card counts as
+2 Tao Lu actions." fontsize="26" flexibleheight="1" flexiblewidth="1" />
+<tmpro text="Gain 1{combat} for each Tao Lu action you have in play." fontsize="24" flexibleheight="1" flexiblewidth="1" />
+</vlayout>
+        ]]
     })
-    local counter_name = "cobra_fang_last_custom_value"
-    local trigger_name = "cobra_fang_add_one_damage"
-    local trigger2_name = "cobra_fang_add_two_damage"
-    return createItemDef({
-        id = "monk_cobra_fang",
-        name = "Horn of Ascendance",
-        acquireCost = 0,
-        types = { actionType, noStealType, monkType, taoLuType },
-        factions = {},
-        playLocation = castPloc,
-        layout = cardLayout,
+    local selector = selectLoc(currentCastLoc).exclude(selectSource()).where(isCardType(taoLuType).And(isCardAction()))
+    local buff = createGlobalBuff({
+        id = card_name .. "_buff",
+        name = "Cobra Fang",
         abilities = {
-            createAbility({
-                id = "monk_cobra_fang_on_play_ability",
-                trigger = autoTrigger,
-                activations = singleActivation,
-                cost = noCost,
-                effect = gainCombatEffect(getCustomValue(currentPid))
-                    .seq(resetCounterEffect(counter_name))
-                    .seq(incrementCounterEffect(counter_name, getCustomValue(currentPid)))
-                    .seq(gainCustomValueEffect(2))
-            }),
-            -- We break up the detect and the gain combat ability so that
-            -- this could work properly if you had multiple copies of this
-            -- card in play at the same time.
-            createAbility({
-                id = "monk_cobra_incremental_damage_ability",
-                trigger = abilityTrigger(trigger_name),
-                activations = multipleActivations,
-                cost = noCost,
-                effect = gainCombatEffect(1)
-            }),
-            createAbility({
-                id = "monk_cobra_incremental_damage_ability2",
-                trigger = abilityTrigger(trigger2_name),
-                activations = multipleActivations,
-                cost = noCost,
-                effect = gainCombatEffect(2)
-            }),
-            createAbility({
-                id = "monk_cobra_fang_ability",
-                trigger = autoTrigger,
-                activations = multipleActivations,
-                cost = noCost,
-                check = getCustomValue(currentPid).gte(getCounter(counter_name).add(1)),
-                -- For QOL if we gain 2 Tao Lu from 1 action we show a +2 animation instead of 2 +1
-                -- animations.
-                effect = ifElseEffect(getCustomValue(currentPid).gte(getCounter(counter_name).add(2)),
-                    incrementCounterEffect(counter_name, 2).seq(fireAbilityTriggerEffect(trigger2_name)),
-                    incrementCounterEffect(counter_name, 1).seq(fireAbilityTriggerEffect(trigger_name)))
+            sacrificeSelfOnTurnEndAbility(card_name .. "_buff_self_sacrifice")
+        },
+        cardEffectAbilities = {
+            createCardEffectAbility({
+                id = card_name .. "_card_effect_ability",
+                trigger = locationChangedCardTrigger,
+                effect = filterTargets(selector, ignoreTarget(gainCombatEffect(1)))
             })
         }
+    })
+    return createActionDef({
+        id = card_name,
+        name = "Cobra Fang",
+        types = { monkType, taoLuType },
+        tags = { monkGalleryCardTag },
+        abilities = {
+            createAbility({
+                id = card_name .. "_ability",
+                trigger = onPlayTrigger,
+                cost = noCost,
+                effect = gainCombatEffect(getCustomValue(ownerPid))
+                        .seq(gainCustomValueEffect(2))
+                        .seq(createCardEffect(buff, currentBuffsLoc))
+						--firing the ability here gives 1 gold/health if other "winds" are in play already.
+                        .seq(fireAbilityTriggerEffect("monkTaoLuSkillUsedTrigger")),
+                playAllType = playFirstPlayType,
+                tags = { gainCombatTag }
+            }),
+            createAbility({
+                id = card_name .. "_ability_trigger",
+                trigger = abilityTrigger(monkTaoLuSkillUsedTrigger),
+                cost = noCost,
+                activations = multipleActivations,
+                effect = gainCombatEffect(1),
+                tags = { gainHealthTag }
+            })
+        },
+        layout = cardLayout
     })
 end
 --Druid
